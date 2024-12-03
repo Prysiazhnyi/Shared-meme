92ac5864a255d83e3090986f26334dccc7cf4824
sprysiazhnyi <sprysiazhnyi@competo.io>
2024-12-03 19:32:43 +0200

    start

--- Lessons-90/ViewController.swift
+++ Lessons-90/ViewController.swift
@@ -7,13 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet var viewImage: UIImageView!
+    
+    //var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    @IBAction func loadImage(_ sender: Any) {
+        
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    @IBAction func topText(_ sender: Any) {
+        
+    }
+    @IBAction func bottomText(_ sender: Any) {
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        viewImage.alpha = 0
+        viewImage.image = image
+        UIView.animate(withDuration: 2, delay: 0, options: [], animations: {
+            self.viewImage.alpha = 1
+        })
+        
+        viewImage.image = image
+    }
+    
+    
 }
 
